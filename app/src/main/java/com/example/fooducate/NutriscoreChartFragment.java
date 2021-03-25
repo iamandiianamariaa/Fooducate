aa691a0787c4e0bb8ceeaaf93e5187cf4af4ac9b
iamandiianamariaa <iamandiianamariaa@gmail.com>
2021-03-25 19:39:57 +0200

    changed pie chart library

--- app/src/main/java/com/example/fooducate/NutriscoreChartFragment.java
+++ app/src/main/java/com/example/fooducate/NutriscoreChartFragment.java
@@ -1,9 +1,73 @@
 package com.example.fooducate;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.chart.common.listener.Event;
+import com.anychart.chart.common.listener.ListenersInterface;
+import com.anychart.charts.Pie;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NutriscoreChartFragment extends Fragment {
 
     public NutriscoreChartFragment() {
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.nutriscore_fragment_layout, container, false);
+
+        AnyChartView anyChartView = view.findViewById(R.id.any_chart_view);
+
+        Pie pie = AnyChart.pie();
+
+        pie.setOnClickListener(new ListenersInterface.OnClickListener(new String[]{"x", "value"}) {
+            @Override
+            public void onClick(Event event) {
+            }
+        });
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("A", 6371664));
+        data.add(new ValueDataEntry("B", 789622));
+        data.add(new ValueDataEntry("C", 7216301));
+        data.add(new ValueDataEntry("D", 1486621));
+        data.add(new ValueDataEntry("E", 1200000));
+
+        pie.data(data);
+
+        pie.title("Nutriscore report based on your scanned products");
+
+        pie.labels().position("outside");
+
+        pie.legend().title().enabled(true);
+        pie.legend().title()
+                .text("Legend")
+                .padding(0d, 0d, 10d, 0d);
+
+        pie.legend()
+                .position("center-bottom")
+                .itemsLayout(LegendLayout.HORIZONTAL)
+                .align(Align.CENTER);
+
+        anyChartView.setChart(pie);
+        return view;
+    }
+
 }
